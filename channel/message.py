2d66ff5fa1776031f5205a90207619db3c892b13
Berg Virtual <berg@vb.com>
2024-01-27 17:52:45 +0800

    check if the raw_msg is image

--- channel/message.py
+++ channel/message.py
@@ -20,9 +20,22 @@ class Message(BaseModel):
         self._raw_msg = msg
         self.receiver_id = info["wx_id"]
         self.receiver_name = info["wx_name"]
-        self.content = msg["content"].strip()
         self.type = msg["type"]
         self.create_time = msg["time"]
+        # if message type is image
+        if msg["type"] == 3:
+            # encrypted image location 
+            self.content = msg["content"]["detail"].strip()
+            if "@chatroom" in msg["content"]["id1"]:
+                self.is_group = True
+                self.room_id = msg["content"]["id1"]
+                self.sender_id = msg["id1"]
+                self.is_at = f"@{self.receiver_name}" in self.content
+        else:
+            self.is_group = False
+            self.sender_id = msg["wxid"]
+        else:
+            self.content = msg["content"].strip()
         if "@chatroom" in msg["wxid"]:
             self.is_group = True
             self.room_id = msg["wxid"]
